5406162f000071e36b74936272dc07c102bdb1db
merak <rahmadadhisasono@gmail.com>
2022-10-26 18:47:30 +0700

    firs commit program php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello World <br>";
+
+$name = "Rahmad";
+$age = 21 ;
+
+echo "Nama Saya <Strong>$name</strong>, saya berusia $age tahun.<br>";
+
+$nameAdik = "Royhan" ;
+$ageAdik = 19 ;
+
+
+$selisihUmur = $age-$ageAdik ;
+echo "nama adik saya <strong> $nameAdik</strong>, adik saya berusia $ageAdik tahun.<br> Selisih umur saya dan adik saya $selisihUmur";
+
+
+
+?>
